c401c36b2452d0ca0694d7947afffb162918a09e
Chief-A <aathilr7@gmail.com>
2021-08-06 21:23:09 +0530

    Created Alpha class in Package1

---
+++ src/com/mycompany/Classes/AccessLevelModifiers/Package1/Alpha.java
@@ -0,0 +1,9 @@
+package com.mycompany.Classes.AccessLevelModifiers.Package1;
+
+public class Alpha {
+    String a_NoModifier = "Alpha.NoModifier";
+    public String a_Public = "Alpha.Public";
+    protected String a_Protected = "Alpha.Protected";
+    private String a_Private = "Alpha.Private";
+}
+
