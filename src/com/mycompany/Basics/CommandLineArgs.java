3aa13656f6f00d4e7a21d643d2886b976d6c6183
Chief-A <aathilr7@gmail.com>
2021-08-08 23:46:22 +0530

    CommandLine Arguments

---
+++ src/com/mycompany/Basics/CommandLineArgs.java
@@ -0,0 +1,9 @@
+package com.mycompany.Basics;
+
+public class CommandLineArgs {
+    public static void main(String[] args) {
+        for (String str : args) {
+            System.out.println(str);
+        }
+    }
+}
