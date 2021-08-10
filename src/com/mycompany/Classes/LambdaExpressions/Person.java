9b3f96c5afdb2ada65cdd57aeca070c78ce178d8
Chief-A <aathilr7@gmail.com>
2021-08-10 23:56:09 +0530

    Implemented Constructor and List generator.

--- src/com/mycompany/Classes/LambdaExpressions/Person.java
+++ src/com/mycompany/Classes/LambdaExpressions/Person.java
@@ -1,6 +1,8 @@
 package com.mycompany.Classes.LambdaExpressions;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Person {
         public enum SEX {
@@ -11,11 +13,26 @@ public class Person {
         SEX gender;
         int age;
 
+        public Person(String n, SEX sex, int age) {
+                name = n;
+                gender = sex;
+                this.age = age;
+        }
+
         public int getAge() {
                 return this.age;
         }
 
         public void printPerson() {
-
+                System.out.println("Name: " + name + "\t Sex: " + gender + "Age: " + age);
         }
+
+        public static List<Person> constructList() {
+                List<Person> list = new ArrayList<>();
+                list.add(new Person("Person1", SEX.MALE, 10));
+                list.add(new Person("Person2", SEX.FEMALE, 20));
+                list.add(new Person("Person3", SEX.MALE, 30));
+                return list;
+        }
+
 }
