7f685100b783e87e70a46f14f8304f293609d10a
Chief A <aathilr7@gmail.com>
2021-08-11 11:07:45 +0530

    Explanation added

--- src/com/mycompany/Classes/LambdaExpressions/LambdaTest.java
+++ src/com/mycompany/Classes/LambdaExpressions/LambdaTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class LambdaTest {
 
+    //Sample criterion: get person older that given age and do some action(here print) on them.
+    //Drawback of this approach is that you will have to create a new method for each new criterion.
     public void printPersonOlderThan(List<Person> list, int age) {
         for (Person p : list) {
             if (p.getAge() > age) {
