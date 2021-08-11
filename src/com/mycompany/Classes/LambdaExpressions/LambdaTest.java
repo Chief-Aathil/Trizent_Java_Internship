0caf07ecb9c0aecdef61367e1064dd30398265b3
Chief A <aathilr7@gmail.com>
2021-08-11 13:11:10 +0530

    Approach 2 More generic function

--- src/com/mycompany/Classes/LambdaExpressions/LambdaTest.java
+++ src/com/mycompany/Classes/LambdaExpressions/LambdaTest.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 public class LambdaTest {
 
-    //Sample criterion: get person older that given age and do some action(here print) on them.
-    //Drawback of this approach is that you will have to create a new method for each new criterion.
+    /**
+     * Approach1:
+     * Sample criterion: get person older that given age and do some action(here print) on them.
+     * Drawback of this approach is that you will have to create a new method for each new criterion.
+     */
     public void printPersonOlderThan(List<Person> list, int age) {
         for (Person p : list) {
             if (p.getAge() > age) {
@@ -15,9 +18,27 @@ public class LambdaTest {
         }
     }
 
+    /**
+     * Approach2:
+     * Create a more flexible search method.You could create a method that will some arguments to check for a
+     * pattern ( eg: age<x, name starts with 'A', MALE). For simplicity, we are showing function that can take
+     * upper and lower bound of age to search. The drawback of this approach is that we will need to change the
+     * search method whenever we update the Person class(like when we add/ change fields).
+     * This approach is only slightly more generic than printPersonOlderThan in Approach1.
+     */
+    public void printPersonWithinRange(List<Person> list, int lowerAge, int upperAge) {
+        for (Person p : list) {
+            if (p.getAge() >= lowerAge && p.getAge() <= upperAge) {
+                p.printPerson();
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         LambdaTest testObj = new LambdaTest();
         List<Person> personList = Person.constructList();
-        testObj.printPersonOlderThan(personList, 15);
+        //testObj.printPersonOlderThan(personList, 15); //Approach 1
+        testObj.printPersonWithinRange(personList, 15, 40);
     }
 }
