fd6a4ba2f520b163e94e85505af972602dab0f14
Chief A <aathilr7@gmail.com>
2021-08-14 21:10:19 +0530

    Case 2 When one method is overridden already.

--- src/com/mycompany/Inheritance/MethodConflictResolution.java
+++ src/com/mycompany/Inheritance/MethodConflictResolution.java
@@ -20,11 +20,43 @@ class Cat extends Mammal implements Animal {
 
 }
 
+
+
+/*
+Case 2: When supertypes share a common ancestor.
+Methods that are already overridden are ignored
+ */
+
+interface WildAnimal extends Animal {
+    default String whoAmI() {
+        return "WildAnimal";
+    }
+}
+
+interface FourLeggedAnimal extends Animal {
+}
+
+class Lion implements WildAnimal, FourLeggedAnimal {
+    /*
+    Here the supertypes `WildAnimal` and `FourLeggedAnimal` both have whoAmI().
+     */
+}
+
+
 public class MethodConflictResolution {
     public static void main(String[] args) {
 
         //Case1:
         Cat cat = new Cat();
         System.out.println(cat.whoAmI());
+
+        //Case 2:
+        /*
+        Here, FourLeggedAnimal's version of whoAmI() returns "Animal"(through inheritance)
+        but WildAnimal's version returns "WildAnimal"(by overriding the supertype method).
+        Since FourLeggedAnimal's version is already overridden in WildAnimal, WildAnimal's version is preferred.
+         */
+        Lion lion = new Lion();
+        System.out.println("\n" + lion.whoAmI());
     }
 }
