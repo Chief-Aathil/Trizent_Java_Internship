a454564d0a7d9db13ff622ca95d599d2daf0af22
Chief A <aathilr7@gmail.com>
2021-08-16 13:36:07 +0530

    Pattern.matches() test

---
+++ src/com/mycompany/Regex/MatchesTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.Regex;
+
+import java.util.regex.Pattern;
+
+public class MatchesTest {
+    public static void main(String[] args) {
+        String regex = "[abc]";
+        String input = "xaz";
+        System.out.println("matches: " + Pattern.matches(regex, input));
+        //Pattern.matches() returns true when the entire input matches the regex
+    }
+}
