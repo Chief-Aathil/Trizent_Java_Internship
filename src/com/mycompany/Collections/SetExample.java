c22378ac22a875cfd9119d2767d9cc93a38383b7
Chief A <aathilr7@gmail.com>
2021-08-19 23:01:02 +0530

    Using Stream to instantiate Set

--- src/com/mycompany/Collections/SetExample.java
+++ src/com/mycompany/Collections/SetExample.java
@@ -9,8 +9,8 @@ public class SetExample {
         Integer[] arr = {1, 3, 10, 9, 2, 8, 5, 4, 6, 7};
 
         Set<Integer> mySet1 = new HashSet<>(Arrays.asList(arr));
-        Set<Integer> mySet2 = new TreeSet<>(Arrays.asList(arr));
-        Set<Integer> mySet3 = new LinkedHashSet<>(Arrays.asList(arr));
+        Set<Integer> mySet2 = new TreeSet<>(mySet1);
+        Set<Integer> mySet3 = mySet1.stream().collect(Collectors.toSet()); // using stream
         mySet1.add(0);
         System.out.println("mySet1(HashSet):");
         for (Integer i : mySet1) {
@@ -24,5 +24,6 @@ public class SetExample {
         for (Integer i : mySet3) {
             System.out.print(i + " ");
         }
+        System.out.println("\n" + (mySet3 instanceof HashSet));
     }
 }
