313654693850ece958af44793b0f3eabad15e70e
Chief A <aathilr7@gmail.com>
2021-08-26 23:39:55 +0530

    Unchecked cast examples

--- src/com/mycompany/Generics/Properties.java
+++ src/com/mycompany/Generics/Properties.java
@@ -1,6 +1,7 @@
 package com.mycompany.Generics;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Properties {
@@ -9,10 +10,26 @@ public class Properties {
         List<String> strList = new ArrayList<String>();
         List<Integer> intList = new ArrayList<Integer>();
         System.out.println(strList.getClass() == intList.getClass());
-        
+        //All instances of a generic class have the same run-time class.
     }
+
+    public static void testInstanceOf() {
+        Collection cs = new ArrayList<String>();
+//        System.out.println(cs instanceof Collection<String>);
+//    Illegal
+
+        Collection<String> cstr = (Collection<String>) cs;
+        //Unchecked cast
+    }
+
+    public static <T> T testCast(T t, Object o) {
+        return (T) o;
+        //unchecked cast
+    }
+
 
     public static void main(String[] args) {
         testSharing();
+        System.out.println(testCast("str", 123)); // gives ClassCastException
     }
 }
