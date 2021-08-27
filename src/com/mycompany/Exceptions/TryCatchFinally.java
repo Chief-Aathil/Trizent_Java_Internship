36d9ef27e28a268e4e2316285337241b52c65d53
Chief A <aathilr7@gmail.com>
2021-08-27 13:48:19 +0530

    Finally block included

--- src/com/mycompany/Exceptions/TryCatchFinally.java
+++ src/com/mycompany/Exceptions/TryCatchFinally.java
@@ -6,13 +6,20 @@ import java.io.PrintWriter;
 
 public class TryCatchFinally {
     public static void main(String[] args) {
+        PrintWriter fileOut = null;
         try {
-            PrintWriter fileOut = new PrintWriter(new FileWriter("src"));
+            fileOut = new PrintWriter(new FileWriter("src"));
 
             System.out.println("Test line");//not executed if exception thrown from line above
         } catch (IOException e) {
             System.out.println("Exception caught!!");
             e.printStackTrace();
+            System.out.println("Stack trace printed");
+        } finally {
+            System.out.println("In finally block...");
+            if (fileOut != null) {
+                fileOut.close();
+            }
         }
     }
 }
