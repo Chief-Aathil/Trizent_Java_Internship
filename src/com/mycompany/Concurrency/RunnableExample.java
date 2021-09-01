4dea8de0dbca6a1cedf7c50625c3c75338e8ff5d
Chief A <aathilr7@gmail.com>
2021-09-01 16:29:23 +0530

    More detailed example. Using object of different class.

--- src/com/mycompany/Concurrency/RunnableExample.java
+++ src/com/mycompany/Concurrency/RunnableExample.java
@@ -1,15 +1,25 @@
 package com.mycompany.Concurrency;
 
+class AnotherRunnableExample implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("\nInside thread!\nUsing:" + this.getClass().toString());
+        System.out.println("Thread name:" + Thread.currentThread().getName());
+    }
+}
 
 public class RunnableExample implements Runnable {
     @Override
     public void run() {
-        System.out.println("Inside thread!!");
-        System.out.println("Thread:" + Thread.currentThread().getName());
+        System.out.println("\nInside thread!\nUsing:" + this.getClass().toString());
+        System.out.println("Thread name:" + Thread.currentThread().getName());
     }
 
     public static void main(String[] args) {
         System.out.println("Thread:" + Thread.currentThread().getName());
         new Thread(new RunnableExample()).start();
+        AnotherRunnableExample obj = new AnotherRunnableExample();
+        Thread thread = new Thread(obj);
+        thread.start();
     }
 }
