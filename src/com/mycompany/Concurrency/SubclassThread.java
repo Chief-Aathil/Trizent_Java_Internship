f42455a3b27246333a9e111462a41744a9bd0692
Chief A <aathilr7@gmail.com>
2021-09-01 16:59:04 +0530

    Using same class to create thread.

--- src/com/mycompany/Concurrency/SubclassThread.java
+++ src/com/mycompany/Concurrency/SubclassThread.java
@@ -8,14 +8,19 @@ class MySubClass extends Thread {
     }
 }
 
-public class SubclassThread {
-
+public class SubclassThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Inside: " + getClass());
+        System.out.println("Thread name:" + Thread.currentThread().getName());
+    }
 
     public static void main(String[] args) {
         System.out.println("Inside: SubClassThread");
         System.out.println("Thread name:" + Thread.currentThread().getName());
         MySubClass obj1 = new MySubClass();
         obj1.start();
-
+        SubclassThread obj2 = new SubclassThread();
+        obj2.start();
     }
 }
